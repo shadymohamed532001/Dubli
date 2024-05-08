ed43d7c1ee25c94a76a93c269a4a4391a7f163fc
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2024-05-08 20:21:32 +0300

    Add new SettingsOptionItem widget
    Add a new SettingsOptionItem widget for displaying settings options in a row.

---
+++ lib/feature/setting/ui/widgets/settings_option_item.dart
@@ -0,0 +1,26 @@
+import 'package:dubli/core/utils/app_styles.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../core/utils/app_colors.dart';
+
+class SettingsOptionItem extends StatelessWidget {
+  const SettingsOptionItem({super.key, required this.tittle});
+  final String tittle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          tittle,
+          style: AppStyle.font18Whitemedium,
+        ),
+        const Icon(
+          Icons.arrow_forward_ios,
+          color: ColorManager.whiteColor,
+        )
+      ],
+    );
+  }
+}
