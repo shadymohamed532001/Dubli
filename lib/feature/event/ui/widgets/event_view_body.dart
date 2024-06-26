cd8ad0e7067bbd81af67ba5da6d21a46cee7c8ce
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2024-06-26 13:10:11 +0300

    ......

--- lib/feature/event/ui/widgets/event_view_body.dart
+++ lib/feature/event/ui/widgets/event_view_body.dart
@@ -84,111 +84,128 @@ class _EventViewBodyState extends State<EventViewBody> {
                 SliverList.builder(
                   itemCount: state.events.length,
                   itemBuilder: (context, index) {
-                    return Container(
+                    final startTime = DateTime.parse(
+                        state.events[index]['fields']['startTime']);
+                    final endTime = DateTime.parse(
+                        state.events[index]['fields']['endTime']);
+
+                    final startTimeFormatted =
+                        DateFormat.Hm().format(startTime);
+                    final endTimeFormatted = DateFormat.Hm().format(endTime);
+                    return Padding(
                       padding: const EdgeInsets.symmetric(
-                        horizontal: 16,
-                        vertical: 10,
+                        vertical: 8,
+                        horizontal: 4,
                       ),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(16),
-                        border: Border.all(
-                          color: ColorManager.darkGreyColor,
-                          width: 2,
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 10,
                         ),
-                        color: Colors.transparent,
-                      ),
-                      child: Row(
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                state.events[index]['name'],
-                                style: AppStyle.font16Whitesemibold,
-                              ),
-                              const SizedBox(
-                                height: 5,
-                              ),
-                              Row(
-                                children: [
-                                  Image.asset(
-                                    ImagesAssetsManager.applogoImage,
-                                    width: 50,
-                                    height: 50,
-                                  ),
-                                  const SizedBox(
-                                    width: 10,
-                                  ),
-                                  Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        '${state.events[index]['fields']['startTime'].toString().split('T')[0].toString()} | ${state.events[index]['fields']['endTime'].toString().split('T')[0].toString()}',
-                                        style: AppStyle.font14Greyregular,
-                                      ),
-                                      const SizedBox(
-                                        height: 5,
-                                      ),
-                                      Text(
-                                        state.events[index]['fields']
-                                            ['description'],
-                                        style: AppStyle.font16Whitesemibold,
-                                      ),
-                                    ],
-                                  )
-                                ],
-                              ),
-                              const SizedBox(
-                                height: 5,
-                              ),
-                              Row(
-                                children: [
-                                  Text(
-                                    state.events[index]['fields']['reminder'],
-                                    style: AppStyle.font14Greyregular,
-                                  ),
-                                     Text(
-                                        '${state.events[index]['fields']['startTime'].toString().split('T')[0].toString()} | ${state.events[index]['fields']['endTime'].toString().split('T')[0].toString()}',
-                                        style: AppStyle.font14Greyregular,
-                                      ),
-                                ],
-                              ),
-                            ],
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16),
+                          border: Border.all(
+                            color: ColorManager.darkGreyColor,
+                            width: 2,
                           ),
-                          const Spacer(),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 16, vertical: 10),
-                            child: Column(
+                          color: Colors.transparent,
+                        ),
+                        child: Row(
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
                               children: [
-                                GestureDetector(
-                                  // onTap: () => _showUpdateDialog(
-                                  //   context,
-                                  //   widget.taskGroupModel,
-                                  //   widget.allTaskModel,
-                                  // ),
-                                  child: const Icon(
-                                    Icons.edit,
-                                    color: ColorManager.whiteColor,
-                                    size: 20,
-                                  ),
+                                Text(
+                                  state.events[index]['name'],
+                                  style: AppStyle.font16Whitesemibold,
                                 ),
-                                const SizedBox(height: 10),
-                                GestureDetector(
-                                  // onTap: () =>
-                                  //     _showDeleteConfirmationDialog(context),
-                                  child: const Icon(
-                                    Icons.delete,
-                                    color: ColorManager.whiteColor,
-                                    size: 20,
-                                  ),
+                                const SizedBox(
+                                  height: 5,
+                                ),
+                                Row(
+                                  children: [
+                                    Image.asset(
+                                      ImagesAssetsManager.applogoImage,
+                                      width: 50,
+                                      height: 50,
+                                    ),
+                                    const SizedBox(
+                                      width: 10,
+                                    ),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          '${state.events[index]['fields']['startTime'].toString().split('T')[0].toString()} | ${state.events[index]['fields']['endTime'].toString().split('T')[0].toString()}',
+                                          style: AppStyle.font14Greyregular,
+                                        ),
+                                        const SizedBox(
+                                          height: 5,
+                                        ),
+                                        Text(
+                                          state.events[index]['fields']
+                                              ['description'],
+                                          style: AppStyle.font16Whitesemibold,
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                                const SizedBox(
+                                  height: 5,
+                                ),
+                                Row(
+                                  children: [
+                                    Text(
+                                      state.events[index]['fields']['reminder'],
+                                      style: AppStyle.font14Greyregular,
+                                    ),
+                                    const SizedBox(
+                                      width: 10,
+                                    ),
+                                    Text(
+                                      '$startTimeFormatted | $endTimeFormatted',
+                                      style: AppStyle.font14Greyregular,
+                                    ),
+                                  ],
                                 ),
                               ],
                             ),
-                          ),
-                        ],
+                            const Spacer(),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 16, vertical: 10),
+                              child: Column(
+                                children: [
+                                  GestureDetector(
+                                    // onTap: () => _showUpdateDialog(
+                                    //   context,
+                                    //   widget.taskGroupModel,
+                                    //   widget.allTaskModel,
+                                    // ),
+                                    child: const Icon(
+                                      Icons.edit,
+                                      color: ColorManager.whiteColor,
+                                      size: 20,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  GestureDetector(
+                                    // onTap: () =>
+                                    //     _showDeleteConfirmationDialog(context),
+                                    child: const Icon(
+                                      Icons.delete,
+                                      color: ColorManager.whiteColor,
+                                      size: 20,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     );
                   },
